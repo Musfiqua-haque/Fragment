169640459fd23908fd9a63686039796596bdd69b
Musfiqua haque <zmushfiqua@gmail.com>
2022-07-31 02:30:01 +0600

    using fragment if you dont want to create nwe element

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <table>
+        <tr>
+            <div>
+                <td>Musfiqua Haque</td>
+                <td>age 24</td>
+            </div>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export default App
